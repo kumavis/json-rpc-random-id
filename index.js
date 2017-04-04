1acb53471bf918fd0b49ab4f7bdb2880d5ef9327
kumavis <aaron@kumavis.me>
2017-04-04 12:37:13 -0700

    use es5 bc js ecosystem is broken

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = IdIterator
 
 function IdIterator(opts){
   opts = opts || {}
-  let max = opts.max || Number.MAX_SAFE_INTEGER
-  let idCounter = typeof opts.start !== 'undefined' ? opts.start : Math.floor(Math.random() * max)
+  var max = opts.max || Number.MAX_SAFE_INTEGER
+  var idCounter = typeof opts.start !== 'undefined' ? opts.start : Math.floor(Math.random() * max)
 
   return function createRandomId () {
     idCounter = idCounter % max
